cc1b26036efed8852bcfb6fbc6de068ca0fe80ef
yantongbin <1317541371@qq.com>
2019-04-11 19:16:42 +0800

    手机轮播图完成

---
+++ src/components/banner.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <section id="banner_wrapper">
+      <div class="swiper-container">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide"
+               v-for="item in banners"
+               :key = "item.bannerId"
+          >
+            <img :src="item.imgUrl" alt>
+          </div>
+          <!-- <div class="swiper-slide">
+            <img src="../style/images/img/banner2.jpg" alt>
+          </div>
+          <div class="swiper-slide">
+            <img src="../style/images/img/banner3.jpg" alt>
+          </div> -->
+        </div>
+        <!-- 如果需要分页器 -->
+        <div class="swiper-pagination"></div>
+      </div>
+    </section>
+  </div>
+</template>
+<script>
+  import Swiper from 'swiper';
+  import 'swiper/dist/css/swiper.css';
+  export default{
+    props:{
+      banners:{
+        type:Array,
+      }
+    },
+    data(){
+      return{
+        swiper: null
+      }
+    },
+    updated(){
+      if(!this.swiper){
+        this.swiper = new Swiper('.swiper-container',{
+          loop:true,
+          pagination: {
+          el: '.swiper-pagination',
+          },
+        })
+      }
+    }
+  } 
+</script>
+
+<style lang="scss" scoped>
+#banner_wrapper {
+  width: 100%;
+  height: 2rem;
+  .swiper-container {
+    width: 100%;
+    height: 100%;
+    .swiper-slide {
+      width: 100%;
+      height: 100%;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+}
+</style>
+
--- src/views/fristPage.vue
+++ src/views/fristPage.vue
@@ -1,5 +1,44 @@
 <template>
     <div>
-        <p>我是首页</p>
+        <Banner :banners='bannerList'></Banner>
     </div>
 </template>
+<script>
+    import Banner from '../components/banner.vue';
+    import axios from 'axios';
+    export default{
+        data(){
+            return {
+                bannerList: []
+            }
+        },
+        components:{
+            Banner
+        },
+        methods:{
+            /**
+             * 请求轮播图的数据
+             */
+            getBannerList(){
+                axios.get('https://m.maizuo.com/gateway?type=2&cityId=220100&k=4948649',{
+                    headers: {
+                        'X-Client-Info': '{"a":"3000","ch":"1002","v":"1.0.0","e":"15547249332104533975391"}',
+                        'X-Host': 'mall.cfg.common-banner'                        
+                    }
+                }).then(res => {
+                    console.log(res.data);
+                    let data = res.data
+                    if(data.status===0){
+                        this.bannerList = data.data
+                    }else{
+                        alert(res.msg)
+                    }
+                })
+            }
+        },
+        created () {
+            this.getBannerList();
+        }
+    }
+</script>
+
